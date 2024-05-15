fc2b015f2cd1da384ec4dce56d44f5a58a25f8f1
Shilin Maksim <shilinmax@gmail.com>
2024-05-15 12:09:36 +0300

    add eslint

--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,3 @@
 import readlineSync from 'readline-sync';
 
-export default () =>  readlineSync.question("Welcome to the Brain Games!\nMay I have your name? ");
-
+export default () => readlineSync.question('Welcome to the Brain Games!\nMay I have your name?');
